e35e1141fe85af22cd7dc69367c265dbd7c64f22
SuperJMN <superjmn@outlook.com>
2025-07-14 01:09:21 +0200

    Works?

--- build/Build.cs
+++ build/Build.cs
@@ -5,6 +5,7 @@ using Nuke.Common.IO;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
+using Serilog;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [AzurePipelines(
@@ -45,21 +46,82 @@ class Build : NukeBuild
             DotNetPack(s => s
                 .SetProject(SourceDirectory / "DeflateBlockCompressor" / "DeflateBlockCompressor.csproj")
                 .SetConfiguration(Configuration)
-                .SetOutputDirectory(ArtifactsDirectory)
                 .SetVersion(GitVersion.MajorMinorPatch)
+                .SetOutputDirectory(ArtifactsDirectory)
                 .SetProperty("PackageReleaseNotes", $"Built from commit {GitVersion.Sha[..7]}"));
+        });
+
+    Target PublishArtifacts => d => d
+        .DependsOn(Pack)
+        .OnlyWhenStatic(() => IsServerBuild)
+        .Executes(() =>
+        {
+            var packages = ArtifactsDirectory.GlobFiles("*.nupkg");
+            Log.Information("Publishing {Count} artifacts to Azure Pipelines: {Packages}", packages.Count, packages);
+            
+            if (packages.Count == 0)
+            {
+                Log.Warning("No .nupkg files found in artifacts directory");
+                return;
+            }
+
+            // Upload artifacts for Azure Pipelines with proper parameters
+            AzurePipelines.Instance?.UploadArtifacts("packages", "packages", ArtifactsDirectory);
         });
     
     Target Publish => d => d
-        .DependsOn(Pack)
+        .DependsOn(PublishArtifacts)
         .Requires(() => NuGetApiKey)
         .OnlyWhenStatic(() => Repository.IsOnMainOrMasterBranch())
         .Executes(() =>
         {
             var packages = ArtifactsDirectory.GlobFiles("*.nupkg");
+            Log.Debug("Found {Count} packages to publish: {Packages}", packages.Count, packages);
+            
+            if (packages.Count == 0)
+            {
+                Log.Error("No .nupkg files found for publishing. Make sure Pack target was executed.");
+                return;
+            }
             
             packages.ForEach(package =>
             {
+                Log.Information("Publishing package: {Package}", package);
+                DotNetNuGetPush(s => s
+                    .SetTargetPath(package)
+                    .SetSource(NuGetSource)
+                    .SetApiKey(NuGetApiKey)
+                    .SetSkipDuplicate(true));
+            });
+        });
+
+    Target PublishOnly => d => d
+        .Requires(() => NuGetApiKey)
+        .OnlyWhenStatic(() => Repository.IsOnMainOrMasterBranch())
+        .Executes(() =>
+        {
+            // This target assumes packages are already in artifacts directory
+            // Use this when you want to publish without rebuilding
+            var packages = ArtifactsDirectory.GlobFiles("*.nupkg");
+            
+            if (packages.Count == 0)
+            {
+                // Try to find packages in source directories as fallback
+                packages = SourceDirectory.GlobFiles("**/bin/**/*.nupkg");
+                Log.Information("No packages in artifacts, found {Count} in source directories", packages.Count);
+            }
+            
+            Log.Information("Found {Count} packages to publish: {Packages}", packages.Count, packages);
+            
+            if (packages.Count == 0)
+            {
+                Log.Error("No .nupkg files found. Run Pack target first or ensure packages exist.");
+                return;
+            }
+            
+            packages.ForEach(package =>
+            {
+                Log.Information("Publishing package: {Package}", package);
                 DotNetNuGetPush(s => s
                     .SetTargetPath(package)
                     .SetSource(NuGetSource)
